1605b9d1fdb3bb30f2fa50919faba79951e432cb
xukai <xukai19871105@126.com>
2019-12-21 21:10:35 +0800

    增加Date类型 SQL语句

--- src/main/java/com/example/student/StudentMapper.java
+++ src/main/java/com/example/student/StudentMapper.java
@@ -1,7 +1,9 @@
 package com.example.student;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 @Mapper
@@ -12,4 +14,12 @@ public interface StudentMapper {
      * @return
      */
     List<StudentDO> findAllStudents();
+
+    /**
+     * 查找同学的生日
+     * @param from
+     * @param end
+     * @return
+     */
+    List<StudentDO> findStudentsByBirthdayRange(@Param("from") Date from, @Param("end") Date end);
 }
--- src/test/java/com/example/StudentSQLTest.java
+++ src/test/java/com/example/StudentSQLTest.java
@@ -2,17 +2,26 @@ package com.example;
 
 import com.example.student.StudentDO;
 import com.example.student.StudentMapper;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.assertj.core.util.DateUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {TestApplication.class})
 public class StudentSQLTest {
+
+    private static Logger log = LoggerFactory.getLogger(StudentSQLTest.class);
 
     @Autowired
     StudentMapper studentMapper;
@@ -20,6 +29,24 @@ public class StudentSQLTest {
     @Test
     public void testCommon() {
         List<StudentDO> studentDOList = studentMapper.findAllStudents();
+        for (StudentDO studentDO : studentDOList) {
+            System.out.println(studentDO.toString());
+        }
+    }
+
+    @Test
+    public void testBirthdayRange() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1990, 0,1);
+        Date from = calendar.getTime();
+        Date end = DateUtils.addYears(from, 1);
+
+        String fromDateString = DateFormatUtils.format(from, "yyyy-MM-dd");
+        String endDateString = DateFormatUtils.format(end, "yyyy-MM-dd");
+
+        log.info("from {} to {}", fromDateString, endDateString);
+
+        List<StudentDO> studentDOList = studentMapper.findStudentsByBirthdayRange(from, end);
         for (StudentDO studentDO : studentDOList) {
             System.out.println(studentDO.toString());
         }
